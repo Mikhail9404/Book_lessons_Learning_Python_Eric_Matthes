ddb027264b683ad6daa6e487d6a2da5506fd369c
Mikhail Kuznetsov <misha9404@yandex.ru>
2023-06-03 19:22:41 +0300

    Exercise 12.6

---
+++ book_lessons_chapter_12/book_lessons_12.6/settings.py
@@ -0,0 +1,18 @@
+class Settings():
+
+    def __init__(self):
+        """Инициализирует настройки игры."""
+        # Параметры экрана.
+        self.screen_width = 1900
+        self.screen_height = 950
+        self.bg_color = (230, 230, 230)
+
+        # Параметры снаряда.
+        self.bullet_speed = 1
+        self.bullet_width = 15
+        self.bullet_height = 3
+        self.bullet_color = (60, 60, 60)
+        self.bullet_allowed = 6
+
+        # Настройка скорости корабля.
+        self.ship_speed = 1.5
